064976284ad97d9f0536bf1494c564e8ce363de1
Jahoon Lee <aoleejohn@naver.com>
2022-07-24 05:13:45 +0900

    first commit

---
+++ 0x00/num10871.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,x,num;
+    cin >> n >> x;
+    while(n--)
+    {
+        cin >> num;
+        if(num<x) cout << num << " ";
+    }
+    return 0;
+}
