b33da77ff283eea26ff6527403e5b027766059f1
Jahoon Lee <aoleejohn@naver.com>
2022-07-23 17:49:45 +0900

    first commit

---
+++ 0x00/14:42.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+// 시간복잡도 oN
+int func1(int n)
+{
+    int sum=0;
+    for(int i=3;i<=n;i++)
+    {
+        if(i%3==0 || i%5==0) sum+=i;
+    }
+    return sum;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << func1(n);
+    
+    return 0;
+}
