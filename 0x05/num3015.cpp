ea16638f4220ba0cf86455f6e166f218ac0ebfba
Jahoon Lee <aoleejohn@naver.com>
2022-07-31 06:03:31 +0900

    first commit

--- 0x05/num3015.cpp
+++ 0x05/num3015.cpp
@@ -1,34 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+  ios_base::sync_with_stdio(0);
+  int n;
+  cin>>n;
 
-    stack<int> st;
-    int n,num,flag=2;
-    long long cnt=0;
-    cin >> n;
-    for(int i=0;i<n;i++){
-        cin >> num;
-        while(!st.empty()){
-            if(num>st.top()){
-                st.pop();
-                flag=1;
-                cnt++;
-            }
-            else if(num==st.top()){
-                cnt+=flag++;
-                break;
-            }
-            else{
-                flag=2;
-                cnt++;
-                break;
-            }
-        }
-        st.push(num);
+  stack<pair<int,int>> st;
+  int num ,cnt_same_height=0;
+  long long  cnt_pair=0;
+  
+  for(int i=0; i<n ; ++i){
+    cin>>num;
+
+    cnt_same_height=1;
+    while(!st.empty() && st.top().first < num){
+      cnt_pair += st.top().second;
+      st.pop();
     }
-    cout << cnt;
 
-    return 0;
+    if(!st.empty() ){
+      if(st.top().first== num){
+        cnt_pair += st.top().second;
+        cnt_same_height = (st.top().second +1 );
+        if(st.size() >1)
+          ++cnt_pair;
+
+        st.pop();
+      }
+      else{
+        ++cnt_pair;
+      }
+    }
+    
+    st.push(make_pair(num, cnt_same_height));
+  }
+
+  cout<<cnt_pair<<'\n';
+  return 0;
 }
