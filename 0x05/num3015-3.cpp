ee41a9e1da90f70e975b4d6ebab7d0e47fa53b45
Jahoon Lee <aoleejohn@naver.com>
2022-08-01 20:31:19 +0900

    first commit

--- 0x05/num3015-3.cpp
+++ 0x05/num3015-3.cpp
@@ -4,22 +4,28 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n,num,cnt=0;
+    int n,num;
+    long long cnt=0;
     stack<pair<int,int>> st;
+    cin >> n;
     for(int i=0;i<n;i++){
         cin >> num;
-        int same_height_cnt=2;
-        while(!st.empty()&&st.top()<num){
+        int same_height_cnt=1;
+        while(!st.empty()&&st.top().first<num){
             cnt += st.top().second;
             st.pop();
         }
         if(!st.empty()){
-            if(st.top==num){
+            if(st.top().first==num){
                 cnt +=st.top().second;
-                
+                same_height_cnt = st.top().second +1;
+                if(st.size()>1) cnt++;
+                st.pop();
             }
+            else cnt++;
         }
+        st.push(pair<int,int>(num,same_height_cnt));
     }
-
+    cout << cnt;
     return 0;
 }
