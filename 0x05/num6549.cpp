5ba89c5efb0108dee70f756b073ba4f1eed946cb
Jahoon Lee <aoleejohn@naver.com>
2022-08-01 22:41:54 +0900

    first commit

--- 0x05/num6549.cpp
+++ 0x05/num6549.cpp
@@ -1,35 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
+int h[100000];
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n,num,min=100001;
-    stack<pair<int,int>> st;
-    while(1){
-        cin >> n;
-        if(n==0) break;
-        for(int i=0;i<n;i++){
-            long long res=1;
-            cin >> num;
-            if(num<min) min = num;
-            while(!st.empty()&&st.top().first+st.top().second<num){
-                st.pop();
+    int n;
+    cin >> n;
+    while(n){
+        stack<int> s;
+        for (int i = 0; i < n; i++) cin >> h[i];
+        long long area = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            while (!s.empty() && h[s.top()] > h[i]) {
+                long long height = h[s.top()];
+                s.pop();
+                long long width = i;
+                if (!s.empty()) width = i - s.top() - 1;
+                if (width*height > area) area = width * height;
             }
-            if(!st.empty()){
-                if(st.top().first<=num){
-                    res = st.top().first+st.top().second;
-                }
-                else{
-
-                }
-            }
-
-
-            if(st.empty()) res *= num;
-            st.push(pair<int,int>(num,res))
+            s.push(i);
         }
-        cout << res;
+ 
+ 
+        while (!s.empty()) {
+            long long height = h[s.top()];
+            s.pop();
+            long long width = n;
+            if (!s.empty()) width = n - s.top() - 1;
+            if (width*height > area) area = width * height;
+        }
+        cout << area << '\n';
+        cin >> n;
     }
-
     return 0;
 }
