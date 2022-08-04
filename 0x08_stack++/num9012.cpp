5a92e8d38c635bcd3b76f2c7b815f22b1b56cd48
Jahoon Lee <aoleejohn@naver.com>
2022-08-04 22:15:54 +0900

    first commit

--- 0x08_stack++/num9012.cpp
+++ 0x08_stack++/num9012.cpp
@@ -10,15 +10,15 @@ int main(){
     while(t--){
         stack<char> st;
         cin >> s;
-        for(int i=0;i<s.length();i++){
-            if(!st.empty() && st.top() != s[i]){
+        for(auto c : s){
+            if(!st.empty() && st.top() != c){
                 st.pop();
             }
-            else if(st.empty() && s[i] == ')'){
-                st.push(s[i]);
+            else if(st.empty() && c == ')'){
+                st.push(c);
                 break;
             }
-            else st.push(s[i]);
+            else st.push(c);
         }
         if(st.empty()) cout << "YES" << "\n";
         else cout << "NO" << "\n";
