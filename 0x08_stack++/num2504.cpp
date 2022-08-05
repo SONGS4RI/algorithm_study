6cbffbed8d5f3814043771d0f163ff250fda0486
Jahoon Lee <aoleejohn@naver.com>
2022-08-05 21:27:01 +0900

    first commit

---
+++ 0x08_stack++/num2504.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    int temp1=0,temp2=0 res=0;
+    stack<pair<char,int>> st;
+
+    cin >> s;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='(' || s[i]=='[') st.push({s[i],i});
+        else if(st.empty()){
+            if(s[i]==')' || s[i]==']'){
+                st.push({s[i],i});
+                break;
+            }
+        }
+        else if(!st.empty() && s[i]==')'){
+            if(i-st.top().second==1 && st.top().first == '(') temp += 2;
+            else if(st.top().first == '[') break;
+            else temp *= 2;
+            st.pop();
+        }
+        else if(!st.empty() && s[i]==']'){
+            if(i-st.top().second==1 && st.top().first == '[') temp += 3;
+            else if(st.top().first == '(') break;
+            else temp *= 3;
+            st.pop();
+        }
+        if(st.empty()){
+            res += temp;
+            temp = 0;
+        }
+    }
+    if(st.empty()) cout << res;
+    else cout << 0;
+
+    return 0;
+}
