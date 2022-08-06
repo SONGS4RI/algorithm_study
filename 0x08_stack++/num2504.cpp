786f4bdc6c81efc934d338c0b7c0b0f44d9ea8c0
Jahoon Lee <aoleejohn@naver.com>
2022-08-06 10:44:00 +0900

    first commit

--- 0x08_stack++/num2504.cpp
+++ 0x08_stack++/num2504.cpp
@@ -1,58 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
+void func(){
+
+}
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     string s;
-    int temp1=0,temp2=0,res=0;
+    int arr[16]={0,}, cnt=0, res=0; // 
     stack<pair<char,int>> st;
 
     cin >> s;
     for(int i=0;i<s.length();i++){
-        if(s[i]=='(' || s[i]=='['){
-            st.push({s[i],i});
-            temp2 += temp1;
-            temp1 =0;
-        }
-        else if(st.empty()){
-            if(s[i]==')' || s[i]==']'){
-                st.push({s[i],i});
-                break;
+        if(s[i]=='('||s[i]=='[') st.push({s[i],i});
+        else if(s[i]==')'){
+            if(!st.empty() && st.top()=='('){
+                if(st.size()>1){
+                    if(i-st.top().second==1) temp1+=2;
+                    else temp1 *=2;
+                }
+                else if(i-st.top().second==1){
+                    res += 2;
+                }
+                else{
+                    res = temp1*2;
+                }
             }
-        }
-        else if(!st.empty() && s[i]==')'){
-            if(i-st.top().second==1 && st.top().first == '('){
-                if(st.size()==1) res += 2;
-                else temp1 += 2;
+            else{
+                cout << 0;
+                exit(0);
             }
-            else if(st.top().first == '[') break;
-            else if(st.size()==1 && st.top().first == '('){
-                res += (temp1+temp2)*2;
-                temp1 = 0;
-                temp2 = 0;
-            }
-            else temp1 *= 2;
-            st.pop();
-            if(st.empty()) temp2 += temp1;
-        }
-        else if(!st.empty() && s[i]==']'){
-            if(i-st.top().second==1 && st.top().first == '['){
-                if(st.size()==1) res += 3;
-                else temp1 += 3;
-            }
-            else if(st.top().first == '(') break;
-            else if(st.size()==1 && st.top().first == '['){
-                res += (temp1+temp2)*2;
-                temp1 = 0;
-                temp2 = 0;
-            }
-            else temp1 *= 3;
-            st.pop();
         }
     }
-    if(st.empty()) cout << res;
-    else cout << 0;
 
     return 0;
 }
