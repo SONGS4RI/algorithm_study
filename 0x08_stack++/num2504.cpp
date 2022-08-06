912ee0fe2ab20e8906cce47cc2491e925794ccd1
Jahoon Lee <aoleejohn@naver.com>
2022-08-06 10:58:41 +0900

    first commit

--- 0x08_stack++/num2504.cpp
+++ 0x08_stack++/num2504.cpp
@@ -8,24 +8,21 @@ int main(){
     cin.tie(NULL);
 
     string s;
-    int arr[16]={0,}, cnt=0, res=0; // 
+    int arr[16]={0,}, res=0; 
     stack<pair<char,int>> st;
 
     cin >> s;
     for(int i=0;i<s.length();i++){
         if(s[i]=='('||s[i]=='[') st.push({s[i],i});
         else if(s[i]==')'){
-            if(!st.empty() && st.top()=='('){
-                if(st.size()>1){
-                    if(i-st.top().second==1) temp1+=2;
-                    else temp1 *=2;
-                }
-                else if(i-st.top().second==1){
-                    res += 2;
+            if(!st.empty() && st.top().first =='('){
+                if(i-st.top().second==1){
+                    arr[st.size()-1] +=2;
                 }
                 else{
-                    res = temp1*2;
+                    arr[st.size()-1] += arr[st.size()]*2; 
                 }
+                st.pop();
             }
             else{
                 cout << 0;
