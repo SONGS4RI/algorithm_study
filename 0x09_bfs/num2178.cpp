643f723bee5b1f49dec00c71fc1a800bd33908c4
Jahoon Lee <aoleejohn@naver.com>
2022-08-07 16:11:25 +0900

    first commit

--- 0x09_bfs/num2178.cpp
+++ 0x09_bfs/num2178.cpp
@@ -1,37 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int m,n,maze[101][101],cnt_min=1;
-    string s;
-    queue<pair<int,int>> qu;
-    int dist[101][101];
-    int dx[4] = {1,0,-1,0};
-    int dy[4] = {0,1,0,-1};
-    cin >> n >> m;
-    for(int i=1;i<=n;i++){
-        cin >> s;
-        for(int j=1;j<=m;j++){
-            maze[i][j] = (int)s[j-1]-48;
-        }
+#define X first
+#define Y second
+string board[102];
+int dist[102][102];
+int n,m;
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> m;
+  for(int i = 0; i < n; i++)
+    cin >> board[i];
+  for(int i = 0; i < n; i++) fill(dist[i],dist[i]+m,-1);
+  queue<pair<int,int> > Q;
+  Q.push({0,0});
+  dist[0][0] = 1;
+  while(!Q.empty()){
+    auto cur = Q.front();
+    Q.pop();
+    for(int dir = 0; dir < 4; dir++){
+      int nx = cur.X + dx[dir];
+      int ny = cur.Y + dy[dir];
+      if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+      if(dist[nx][ny] >= 0 || board[nx][ny] != '1') continue;
+      dist[nx][ny] = dist[cur.X][cur.Y]+1;
+      Q.push({nx,ny});
     }
-    for(int i=1;i<=n;i++) fill(dist[i],dist[i]+m,-1);
-    qu.push({1,1});
-    while(!qu.empty()){
-        pair<int,int> cur = qu.front();
-        qu.pop();
-        for(int i=0;i<4;i++){
-            int nx = cur.first + dx[i];
-            int ny = cur.second + dy[i];
-            if(nx<1 || nx>n || ny<1 || ny>m) continue;
-            if(dist[nx][ny]>=0 || maze[nx][ny]!=1) continue;
-            dist[nx][ny] = dist[cur.first][cur.second]+1;
-            qu.push({nx,ny});
-        }
-        
-    }
-    cout << dist[n][m]+1;
-    return 0;
+  }
+  cout << dist[n-1][m-1]; 
 }
