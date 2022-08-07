74607d1e4a80af380194701be32f74bc496a248f
Jahoon Lee <aoleejohn@naver.com>
2022-08-07 23:02:29 +0900

    first commit

--- 0x09_bfs/num4179.cpp
+++ 0x09_bfs/num4179.cpp
@@ -1,70 +1,66 @@
-#include<iostream>
-#include<queue>
-
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+#define X first
+#define Y second
 
-    int r,c,time[1001][1001];
-    int dx[4] = {1,0,-1,0};
-    int dy[4] = {0,1,0,-1};
-    queue<pair<int,int>> qu;
-    string maze[1001];
-
-    cin >> r >> c;
-    for(int i=0;i<r;i++){
-        cin >> maze[i];
-        fill(time[i],time[i]+c,-1);
-        for(int j=0;j<c;j++){
-            if(maze[i][j]=='J' || maze[i][j]=='F'){
-                time[i][j] = 1;
-                qu.push({i,j});
-            }
-        }
+string board[1002];
+int dist1[1002][1002]; 
+int dist2[1002][1002]; 
+int n, m;
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> m;
+  for(int i = 0; i < n; i++){
+    fill(dist1[i], dist1[i]+m, -1);
+    fill(dist2[i], dist2[i]+m, -1);    
+  }
+  for(int i = 0; i < n; i++)
+    cin >> board[i];  
+  queue<pair<int,int> > Q1;
+  queue<pair<int,int> > Q2;
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+      if(board[i][j] == 'F'){
+        Q1.push({i,j});
+        dist1[i][j] = 0;        
+      }
+      if(board[i][j] == 'J'){
+        Q2.push({i,j});
+        dist2[i][j] = 0;
+      }
     }
-    while(1){
-        if(maze[qu.front().first][qu.front().second]=='F'){
-            auto cur = qu.front();
-            qu.pop();
-            qu.push(cur);
-        }
-        else break;
+  }
+  // 불에 대한 BFS
+  while(!Q1.empty()){
+    auto cur = Q1.front(); Q1.pop();
+    for(int dir = 0; dir < 4; dir++){
+      int nx = cur.X + dx[dir];
+      int ny = cur.Y + dy[dir];
+      if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+      if(dist1[nx][ny] >= 0 || board[nx][ny] == '#') continue; 
+      dist1[nx][ny] = dist1[cur.X][cur.Y]+1;
+      Q1.push({nx,ny});
     }
+  }
 
-    while(!qu.empty()){
-        auto cur = qu.front();
-        qu.pop();
-        if(cur.first==0 || cur.second==0 || cur.first == r-1 || cur.second == c-1){
-            if(maze[cur.first][cur.second]=='j'){
-                cout << time[cur.first][cur.second];
-                return 0;
-            }
-        }
-        for(int i=0;i<4;i++){
-            int nx = cur.first+dx[i];
-            int ny = cur.second+dy[i];
-            
-            if(nx<0 || ny<0 || nx>=r || ny>=c) continue;
-            if(maze[nx][ny]=='#' || maze[nx][ny]=='F') continue;
-            if(maze[cur.first][cur.second]=='F'){
-                if(maze[nx][ny]=='J' || maze[nx][ny]=='.') maze[nx][ny] = 'F';
-            }
-            else if(maze[cur.first][cur.second]=='J' && maze[nx][ny]=='.'){
-                maze[nx][ny] = 'J';
-                time[nx][ny] = time[cur.first][cur.second] +1;
-            }
-            qu.push({nx,ny});
-            if(nx==0 || ny==0 || nx == r-1 || ny == c-1){
-                if(maze[nx][ny]=='J'){
-                    cout << time[nx][ny];
-                    return 0;
-                }
-            }
-        }
-
+  // 지훈이에 대한 BFS
+  while(!Q2.empty()){
+    auto cur = Q2.front(); Q2.pop();
+    for(int dir = 0; dir < 4; dir++){
+      int nx = cur.X + dx[dir];
+      int ny = cur.Y + dy[dir];
+      if(nx < 0 || nx >= n || ny < 0 || ny >= m){ // 범위를 벗어났다는 것은 탈출에 성공했다는 의미. 큐에 거리 순으로 들어가므로 최초에 탈출한 시간을 출력하면 됨.
+        cout << dist2[cur.X][cur.Y]+1; 
+        return 0;
+      }
+      if(dist2[nx][ny] >= 0 || board[nx][ny] == '#') continue;
+      if(dist1[nx][ny] != -1 && dist1[nx][ny] <= dist2[cur.X][cur.Y]+1) continue; // 불의 전파 시간을 조건에 추가
+      dist2[nx][ny] = dist2[cur.X][cur.Y]+1;
+      Q2.push({nx,ny});
     }
-    cout << "IMPOSSIBLE";
-    
-
+  }
+  cout << "IMPOSSIBLE"; // 여기에 도달했다는 것은 탈출에 실패했음을 의미.
 }
