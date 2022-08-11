2069d5f4df4728a72feb67f77eb68258f31ad972
Jahoon Lee <aoleejohn@naver.com>
2022-08-11 20:50:20 +0900

    first commit

--- 0x09_bfs/num5014.cpp
+++ 0x09_bfs/num5014.cpp
@@ -4,17 +4,17 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int f,s,g,u,d,arr[1000001];
+    int f,s,g,u,d,arr[1000001],cur;
     queue<int> q;
     cin >> f >> s >> g >> u >> d;
     fill(arr,arr+f+1,-1);
     arr[s] = 0;
     q.push(s);
     while(!q.empty() && arr[g]==-1){
-        int cur = q.front();
+        cur = q.front();
         q.pop();
         for(int c : {cur+u,cur-d}){
-            if(c<0 || c>f)continue;
+            if(c<=0 || c>f)continue;
             if(arr[c]>=0) continue;
             arr[c] = arr[cur] +1;
             q.push(c);
