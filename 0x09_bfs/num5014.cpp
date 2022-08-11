0f76dc96e7aae0fb02d164232e67ba9e09736c90
Jahoon Lee <aoleejohn@naver.com>
2022-08-11 20:41:57 +0900

    first commit

---
+++ 0x09_bfs/num5014.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int f,s,g,u,d,arr[1000001];
+    queue<int> q;
+    cin >> f >> s >> g >> u >> d;
+    fill(arr,arr+f+1,-1);
+    arr[s] = 0;
+    q.push(s);
+    while(!q.empty() && arr[g]==-1){
+        int cur = q.front();
+        q.pop();
+        for(int c : {cur+u,cur-d}){
+            if(c<0 || c>f)continue;
+            if(arr[c]>=0) continue;
+            arr[c] = arr[cur] +1;
+            q.push(c);
+        }
+    }
+    if(arr[g]!=-1) cout << arr[g];
+    else cout << "use the stairs";
+    return 0;
+}
