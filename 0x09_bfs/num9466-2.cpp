6ed5a0530811bf9a17b8cdc1a9e200436626b871
Jahoon Lee <aoleejohn@naver.com>
2022-08-16 17:47:45 +0900

    first commit

---
+++ 0x09_bfs/num9466-2.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,stu[100001],vis[100001];
+void run(int x){
+    int cur = x;
+    while(true){
+        vis[cur] = x;
+        cur = stu[cur];
+
+        if(vis[cur]==x){
+            while(vis[cur]!=-1){
+                vis[cur] = -1;
+                cur = stu[cur];
+            }
+            return;
+        }
+        else if(vis[cur]!=0) return;
+    }
+
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--){
+        cin >> n;
+        fill(vis+1,vis+n+1,0);
+        for(int i=1;i<=n;i++) cin >> stu[i];
+        for(int i=1;i<=n;i++){
+            if(vis[i]==0) run(i);
+        }
+        int cnt =0;
+        for(int i=1;i<=n;i++){
+            if(vis[i]!=-1) cnt++;
+        }
+        cout << cnt << "\n";
+    }
+
+    return 0;
+}
