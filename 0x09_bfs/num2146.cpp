9f61706745dca87c2e764c7f22da7d4d22b09222
Jahoon Lee <aoleejohn@naver.com>
2022-08-18 18:02:30 +0900

    first commit

--- 0x09_bfs/num2146.cpp
+++ 0x09_bfs/num2146.cpp
@@ -1,20 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n,arr[101][101],num[101][101],dist[101][101],island_num,res=201;
-int island,nx,ny,flag=0;
+int n,arr[101][101],dist[101][101],res=201;
+int island=1,nx,ny,flag=0;
 const int dx[4] = {-1,0,1,0};
 const int dy[4] = {0,1,0,-1};
 pair<int,int> cur;
 queue<pair<int,int>> q;
 
 void bfs(){
-    island = 1;
-    for(int i=0;i<n;i++) fill(num[i],num[i]+n,0);
+    for(int i=0;i<n;i++) fill(dist[i],dist[i]+n,0);
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            if(arr[i][j]==1 && num[i][j]==0){
-                num[i][j] = island;
+            if(arr[i][j]==1 && dist[i][j]==0){
+                arr[i][j] = island;
+                dist[i][j] = 1;
                 q.push({i,j});
                 while(!q.empty()){
                     cur = q.front();
@@ -23,8 +23,9 @@ void bfs(){
                         nx = cur.first + dx[dir];
                         ny = cur.second + dy[dir];
                         if(nx<0 || ny<0 || nx>=n || ny>=n) continue;
-                        if(num[nx][ny]!=0 || arr[nx][ny]!=1) continue;
-                        num[nx][ny] = island;
+                        if(arr[nx][ny]!=1 || dist[nx][ny]!=0) continue;
+                        arr[nx][ny] = island;
+                        dist[nx][ny] = 1;
                         q.push({nx,ny});
                     }                    
                 }
@@ -36,43 +37,44 @@ void bfs(){
 
 void bridge(){
     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            fill(dist[i],dist[i]+n,-1);
-            if(num[i][j]==0){
-                for(int dir=0;dir<4;dir++){                    
-                    nx = i + dx[dir];
-                    ny = j + dy[dir];
-                    if(num[nx][ny]!=0){
-                        island_num = num[nx][ny];
-                        flag=0;
-                        q.push({i,j});
-                        dist[i][j]=1;
-                        while(!q.empty()){
-                            cur = q.front();
-                            q.pop();
-                            if(dist[cur.first][cur.second]>=res) break;
-                            for(int dir=0;dir<4;dir++){
-                                nx = cur.first + dx[dir];
-                                ny = cur.second + dy[dir];
-                                if(nx<0 || ny<0 || nx>=n || ny>=n) continue;
-                                if(num[nx][ny]!=0 && num[nx][ny]!=island_num){
-                                    res = min(res,dist[cur.first][cur.second]);
-                                    flag =1;
-                                    break;
-                                }
-                                if(num[nx][ny]!=0) continue;
-                                dist[nx][ny] = dist[cur.first][cur.second] +1;
-                                q.push({nx,ny});
-                            }     
-                            if(flag==1) break;
+            for(int j=0;j<n;j++){
+                for(int k=0;k<n;k++) fill(dist[k],dist[k]+n,-1);
+                if(arr[i][j]==0){
+                    for(int dir=0;dir<4;dir++){                    
+                        nx = i + dx[dir];
+                        ny = j + dy[dir];
+                        if(nx<0 || ny<0 || nx>=n || ny>=n) continue;
+                        if(arr[nx][ny]!=0){
+                            island = arr[nx][ny];
+                            flag=0;
+                            q.push({i,j});
+                            dist[i][j]=1;
+                            while(!q.empty()){
+                                cur = q.front();
+                                q.pop();
+                                if(dist[cur.first][cur.second]>=res) break;
+                                for(int dir=0;dir<4;dir++){
+                                    nx = cur.first + dx[dir];
+                                    ny = cur.second + dy[dir];
+                                    if(nx<0 || ny<0 || nx>=n || ny>=n) continue;
+                                    if(arr[nx][ny]!=0 && arr[nx][ny]!=island){
+                                        res = min(res,dist[cur.first][cur.second]);
+                                        flag =1;
+                                        break;
+                                    }
+                                    if(dist[nx][ny]>=0 || arr[nx][ny]!=0) continue;
+                                    dist[nx][ny] = dist[cur.first][cur.second] +1;
+                                    q.push({nx,ny});
+                                }     
+                                if(flag==1) break;
+                            }
+                            while(!q.empty()) q.pop();
+                            break;
                         }
-                        while(!q.empty()) q.pop();
-                        break;
                     }
                 }
             }
-        }
-    }
+        }    
 }
 
 int main(){
@@ -85,6 +87,7 @@ int main(){
     }
     bfs();
     bridge();
+    
     cout << res;
 
     return 0;
