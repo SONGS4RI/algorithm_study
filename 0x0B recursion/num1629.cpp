39fc165e9f4518747c1a0c05c69f305062f8e1bd
Jahoon Lee <aoleejohn@naver.com>
2022-09-21 10:52:53 +0900

    mm

--- 0x0B recursion/num1629.cpp
+++ 0x0B recursion/num1629.cpp
@@ -1,18 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+long long func(long long a, long long b, long long c)
+{
+    long long res;
+    if(b==1)
+        return a % c;
+    res = func(a,b/2,c);
+    res = res * res % c;
+    if(b%2 == 0)
+        return res;
+    return res * a % c;
+}
+
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    long long a,b,c,res=1;
-    a = (long long)INT_MAX;
-    b = (long long)INT_MAX;
-    c = 4;
-    // cin >> a >> b >> c;
-    while(b--)
-    {
-        res = (res * a) % c;
-    }
+    long long a,b,c,res;
+    cin >> a >> b >> c;
+    res = func(a,b,c);
     cout << res << '\n';
 }
