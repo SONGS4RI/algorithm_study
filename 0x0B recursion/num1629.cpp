51249b9c75cb90b253c53e544b715cb2b8cb771d
Jahoon Lee <aoleejohn@naver.com>
2022-09-21 09:56:14 +0900

    eee

--- 0x0B recursion/num1629.cpp
+++ 0x0B recursion/num1629.cpp
@@ -6,8 +6,10 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     long long a,b,c,res=1;
-
-    cin >> a >> b >> c;
+    a = (long long)INT_MAX;
+    b = (long long)INT_MAX;
+    c = 4;
+    // cin >> a >> b >> c;
     while(b--)
     {
         res = (res * a) % c;
